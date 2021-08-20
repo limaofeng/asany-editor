01a9b3213a57110c154ba102650101d5179c4cf5
李茂峰 <limaofeng@msn.com>
2021-08-20 19:53:27 +0800

    chore: 消除无效代码

--- tsdx.config.js
+++ tsdx.config.js
@@ -3,8 +3,6 @@ const replace = require('@rollup/plugin-replace');
 
 module.exports = {
   rollup(config, options) {
-    // const external = config.external;
-    // config.external = id => (id.match(/.svg$/) ? false : external(id));
     config.plugins = config.plugins.map((plugin) => {
       if (plugin && plugin.name === 'replace') {
         return replace({
