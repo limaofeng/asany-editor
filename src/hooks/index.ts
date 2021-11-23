a5c047b0e7f206b886780979400169adde9e48ce
李茂峰 <limaofeng@msn.com>
2021-11-23 15:01:56 +0800

    fix: 导出 useTools

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useAsanyStore } from './useAsanyStore';
 export { default as useDispatch } from './useDispatch';
 export { default as useEditor } from './useEditor';
 export { default as useSelector } from './useSelector';
+export { default as useTools } from './useTools';
